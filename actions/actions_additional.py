4ea321253835021de6054ce54b5e512557195478
Bastard12 <rus.akopyan03@mail.ru>
2023-11-24 17:06:54 +0300

    list_of_accounts(moving changes into additional files)

---
+++ actions/actions_additional.py
@@ -0,0 +1,88 @@
+import os
+from typing import Dict, Text, Any, List
+import logging
+from dateutil import parser
+import sqlalchemy as sa
+
+from rasa_sdk.interfaces import Action
+from rasa_sdk.events import (
+    SlotSet,
+    EventType,
+    ActionExecuted,
+    SessionStarted,
+    Restarted,
+    FollowupAction,
+    UserUtteranceReverted,
+)
+from rasa_sdk import Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+from actions.parsing import (
+    parse_duckling_time_as_interval,
+    parse_duckling_time,
+    get_entity_details,
+    parse_duckling_currency,
+)
+
+from actions.profile_db import create_database, ProfileDB
+
+from actions.custom_forms import CustomFormValidationAction
+
+
+logger = logging.getLogger(__name__)
+
+# The profile database is created/connected to when the action server starts
+# It is populated the first time `ActionSessionStart.run()` is called .
+
+PROFILE_DB_NAME = os.environ.get("PROFILE_DB_NAME", "profile")
+PROFILE_DB_URL = os.environ.get("PROFILE_DB_URL", f"sqlite:///{PROFILE_DB_NAME}.db")
+ENGINE = sa.create_engine(PROFILE_DB_URL)
+create_database(ENGINE, PROFILE_DB_NAME)
+
+profile_db = ProfileDB(ENGINE)
+
+NEXT_FORM_NAME = {
+    "pay_cc": "cc_payment_form",
+    "transfer_money": "transfer_money_form",
+    "search_transactions": "transaction_search_form",
+    "check_earnings": "transaction_search_form",
+}
+
+FORM_DESCRIPTION = {
+    "cc_payment_form": "credit card payment",
+    "transfer_money_form": "money transfer",
+    "transaction_search_form": "transaction search",
+}
+
+
+class ActionShowAccounts(Action):
+    """List of the credit card accounts"""
+
+    def name(self) -> Text:
+        """Unique identifier of the action"""
+        return "action_show_accounts"
+
+    async def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        """Executes the custom action"""
+        accounts = profile_db.list_credit_cards(tracker.sender_id)
+        formatted_accounts = "\n" + "\n".join(
+            [f"- {account.title()}" for account in accounts]
+        )
+        dispatcher.utter_message(
+            response="utter_accounts",
+            formatted_accounts = formatted_accounts,
+        )
+
+        events = []
+        active_form_name = tracker.active_form.get("name")
+        if active_form_name:
+            # keep the tracker clean for the predictions with form switch stories
+            events.append(UserUtteranceReverted())
+            # trigger utter_ask_{form}_AA_CONTINUE_FORM, by making it the requested_slot
+            events.append(SlotSet("AA_CONTINUE_FORM", None))
+            # # avoid that bot goes in listen mode after UserUtteranceReverted
+            events.append(FollowupAction(active_form_name))
+
+        return events
